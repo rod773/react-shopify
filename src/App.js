f8b765c9a82b68c4ff34ed729ea5eeb536f3e971
rod773 <rodrigoarcadio1@gmail.com>
2024-06-01 12:13:49 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <h1>React Shopify</h1>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/context/shopContext.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import Client from "shopify-buy";
+
+class shopContext extends Component {
+  render() {
+    return <div>shopContext</div>;
+  }
+}
+
+export default shopContext;
