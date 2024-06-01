29351deef106371d7975f17c72911937032b3896
rod773 <rodrigoarcadio1@gmail.com>
2024-06-01 16:21:52 +0200

    update

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-
 import Client from "shopify-buy";
+
+const ShopContext = React.createContext();
 
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-  domain: "your-shop-name.myshopify.com",
-  storefrontAccessToken: "your-storefront-access-token",
+  domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
 });
 
 class shopProvider extends Component {
