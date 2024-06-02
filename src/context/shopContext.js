304b417c9b9730550bda47205fe5eeef5060437d
rod773 <rodrigoarcadio1@gmail.com>
2024-06-02 16:28:55 +0200

    update

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -19,7 +19,11 @@ class ShopProvider extends Component {
   };
 
   componentDidMount() {
-    this.createCheckout();
+    if (localStorage.checkout_id) {
+      this.fetchCheckout(localStorage.checkout_id);
+    } else {
+      this.createCheckout();
+    }
   }
 
   createCheckout = async () => {
@@ -28,7 +32,11 @@ class ShopProvider extends Component {
     this.setState({ checkout: checkout });
   };
 
-  fetchCheckout = async () => {};
+  fetchCheckout = async (checkoutId) => {
+    client.checkout.fetch(checkoutId).then((checkout) => {
+      this.setState({ checkout: checkout });
+    });
+  };
 
   addItemCheckout = async () => {};
 
