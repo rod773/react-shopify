021fa2dd44cd0ab2ef4ac6076a24c7fa3eff92a2
rod773 <rodrigoarcadio1@gmail.com>
2024-06-02 15:06:50 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import shopProvider from "context/shopContext";
+import ShopProvider from "context/shopContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <shopProvider>
+    <ShopProvider>
       <App />
-    </shopProvider>
+    </ShopProvider>
   </React.StrictMode>
 );
